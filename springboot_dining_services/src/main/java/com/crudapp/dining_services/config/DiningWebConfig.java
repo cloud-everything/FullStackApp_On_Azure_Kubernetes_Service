5d151a38b867c2f04cf7fb0f5859903e6f04ec6f
VISH01AL <g.vish1al@gmail.com>
2024-10-06 18:03:27 -0400

    removed cors from springboot

--- springboot_dining_services/src/main/java/com/crudapp/dining_services/config/DiningWebConfig.java
+++ springboot_dining_services/src/main/java/com/crudapp/dining_services/config/DiningWebConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration //for cors testing in dev environment
+//@Configuration //for cors testing in dev environment
 public class DiningWebConfig implements WebMvcConfigurer{
 	@Value("${cors.allowed.origins}")
     private String[] allowedOrigins;
